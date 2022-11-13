5dbb37f43924905c5b43e9c5f9892f738675f495
Clarks1223 <pablo.uchuari@epn.edu.ec>
2022-11-13 17:22:52 -0500

    Se incluyen los atributos numero del que marca y numero que recibe la llamada

--- src/Principal.java
+++ src/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
         DecimalFormat dcf=new DecimalFormat("0.00");
         llamada llamada1=new llamada();
         System.out.println("Llamada numero 1");
+        System.out.println("Su numero telefonico es: "+llamada1.getNumeroinicial());
+        System.out.println("El numero al que marco es: "+llamada1.getNumerofinal());
         System.out.println("Tiempo de llamada: "+llamada1.getTiempoLlamada()+" minutos");
         System.out.println("Costo por minuto: "+dcf.format(llamada1.getCostoLlamada()) + " ctvs");
         System.out.println("Total a pagar: "+ dcf.format(llamada1.getTotal())+" ctvs");
--- src/llamada.java
+++ src/llamada.java
@@ -2,11 +2,32 @@ public class llamada {
     double tiempoLlamada;
     double costoLlamada;
     double total;
+    String numerofinal;
+
+    String numeroinicial;
 
     public llamada(){
         tiempoLlamada=3.5;
         costoLlamada=0.10;
         total=tiempoLlamada*costoLlamada;
+        numerofinal="0987654321";
+        numeroinicial="1234567890";
+    }
+
+    public String getNumerofinal() {
+        return numerofinal;
+    }
+
+    public void setNumerofinal(String numerofinal) {
+        this.numerofinal = numerofinal;
+    }
+
+    public String getNumeroinicial() {
+        return numeroinicial;
+    }
+
+    public void setNumeroinicial(String numeroinicial) {
+        this.numeroinicial = numeroinicial;
     }
 
     public double getTiempoLlamada() {
