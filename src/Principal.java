185c3ec185a57a92e6f61a279535a74a9a1cb444
Clarks1223 <pablo.uchuari@epn.edu.ec>
2022-11-13 16:39:06 -0500

    se agrega mensajes en la clase principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        llamada llamada1=new llamada();
+        System.out.println("Llamada numero 1");
+        System.out.println("Tiempo de llamada: "+llamada1.getTiempoLlamada()+" minutos");
+        System.out.println("Costo por minuto: "+(llamada1.getCostoLlamada()) + " ctvs");
+        System.out.println("Total a pagar: "+ (llamada1.getTotal())+" ctvs");
     }
 }
---
+++ src/llamada.java
@@ -0,0 +1,35 @@
+public class llamada {
+    double tiempoLlamada;
+    double costoLlamada;
+    double total;
+
+    public llamada(){
+        tiempoLlamada=3.5;
+        costoLlamada=0.10;
+        total=tiempoLlamada*costoLlamada;
+    }
+
+    public double getTiempoLlamada() {
+        return tiempoLlamada;
+    }
+
+    public void setTiempoLlamada(double tiempoLlamada) {
+        this.tiempoLlamada = tiempoLlamada;
+    }
+
+    public double getCostoLlamada() {
+        return costoLlamada;
+    }
+
+    public void setCostoLlamada(double costoLlamada) {
+        this.costoLlamada = costoLlamada;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
