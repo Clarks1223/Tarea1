8402852229a0dec74216f4fb2d12e9a278ac8338
Clarks1223 <pablo.uchuari@epn.edu.ec>
2022-11-13 16:40:53 -0500

    Se agrega decimalformat para controlar los decimales

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,12 @@
+import java.text.DecimalFormat;
+
 public class Principal {
     public static void main(String[] args) {
+        DecimalFormat dcf=new DecimalFormat("0.00");
         llamada llamada1=new llamada();
         System.out.println("Llamada numero 1");
         System.out.println("Tiempo de llamada: "+llamada1.getTiempoLlamada()+" minutos");
-        System.out.println("Costo por minuto: "+(llamada1.getCostoLlamada()) + " ctvs");
-        System.out.println("Total a pagar: "+ (llamada1.getTotal())+" ctvs");
+        System.out.println("Costo por minuto: "+dcf.format(llamada1.getCostoLlamada()) + " ctvs");
+        System.out.println("Total a pagar: "+ dcf.format(llamada1.getTotal())+" ctvs");
     }
 }
